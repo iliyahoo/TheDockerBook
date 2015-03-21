afb0198a6297f95c79f3102a03f88744230abb11
Iliya Strakovich <iliya@strakovich.com>
2015-03-21 23:13:38 +0200

    page 155: before Installing Jenkins CI

---
+++ sinatra/webapp/lib/app.rb
@@ -0,0 +1,26 @@
+require "rubygems"
+require "sinatra"
+require "json"
+require "redis"
+require "uri"
+
+class App < Sinatra::Application
+      uri=URI.parse(ENV['DB_PORT'])
+      redis = Redis.new(:host => uri.host, :port => uri.port)
+
+      set :bind, '0.0.0.0'
+
+      get '/' do
+        "<h1>DockerBook Test Redis-enabled Sinatra app</h1>"
+      end
+
+      get '/json' do
+        params = redis.get "params"
+        params.to_json
+      end
+
+      post '/json/?' do
+        redis.set "params", [params].to_json
+        params.to_json
+      end
+end
